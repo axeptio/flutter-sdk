e7b1a2c3e0503fcd4d50ddc5b2b071bd2464907c
davinncia <mahelucas1@gmail.com>
2024-04-26 17:02:04 +0200

    feat(android): display pop up on initialize

--- android/src/main/kotlin/io/axept/axeptio_sdk/AxeptioSdkPlugin.kt
+++ android/src/main/kotlin/io/axept/axeptio_sdk/AxeptioSdkPlugin.kt
@@ -1,24 +1,43 @@
 package io.axept.axeptio_sdk
 
 import androidx.annotation.NonNull
+import android.app.Activity
 
+import io.flutter.embedding.engine.plugins.activity.ActivityAware
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 
+import io.axept.android.library.AxeptioSDK
+
+
 /** AxeptioSdkPlugin */
-class AxeptioSdkPlugin : FlutterPlugin, MethodCallHandler {
-    /// The MethodChannel that will the communication between Flutter and native Android
-    ///
-    /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-    /// when the Flutter Engine is detached from the Activity
+class AxeptioSdkPlugin : FlutterPlugin, MethodCallHandler, ActivityAware {
     private lateinit var channel: MethodChannel
+    private var activity: Activity? = null
 
     override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
         channel = MethodChannel(flutterPluginBinding.binaryMessenger, "axeptio_sdk")
         channel.setMethodCallHandler(this)
+    }
+
+    override fun onAttachedToActivity(binding: ActivityPluginBinding) {
+        activity = binding.activity
+    }
+
+    override fun onDetachedFromActivityForConfigChanges() {
+        activity = null
+    }
+
+    override fun onDetachedFromActivity() {
+        activity = null
+    }
+
+    override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
+        activity = binding.activity
     }
 
     override fun onMethodCall(call: MethodCall, result: Result) {
@@ -28,6 +47,17 @@ class AxeptioSdkPlugin : FlutterPlugin, MethodCallHandler {
             }
 
             "initialize" -> {
+                val arguments = call.arguments?.let { it as? HashMap<String, Any> } ?: run {
+                    result.error("invalid_args", "Wrong arguments for initialize", null)
+                    return
+                }
+
+                val clientId = arguments.get("clientId") as String
+                val cookiesVersion = arguments.get("cookiesVersion") as String
+                val token = arguments.get("token") as? String?
+
+                AxeptioSDK.instance().initialize(activity!!, clientId, cookiesVersion, token)
+
                 result.success(null)
             }
 
